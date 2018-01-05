a025e8ae2072d427489236cba1e78976e13b1a74
tikTokTik <mithoonk@flock.com>
2018-01-05 17:38:53 +0530

    Data Binding Implement

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 new Vue({
   el:'#widget',
   data:{
-    name:'Mithoon Kumar'
+    name:'Mithoon Kumar',
+    website:"http://www.google.in",
+    tag:"<p>Hello</p>"
   },
   methods:{
     greet:function(text){
       console.log("consoling",this);// look at the object structure of this
-      return "Hey" + " " +text +" "+this._data.name + " "+ this.name ; 
+      return "Hey" + " " +text +" "+this._data.name + " "+ this.name ;
 
     }
   }
