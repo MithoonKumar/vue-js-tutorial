8710731d2d2734a6b57f9b69563b6cc11a71a272
tikTokTik <mithoonk@flock.com>
2018-01-17 18:10:22 +0530

    Example of event handling in vuejs

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ new Vue({
     greet:function(text){
       console.log("consoling",this);// look at the object structure of this
       return "Hey" + " " +text +" "+this._data.name + " "+ this.name ;
-
+    },
+    changeUrl:function(){
+      this.website = "https://atom.io/packages/terminal-plus";
     }
   }
 });
