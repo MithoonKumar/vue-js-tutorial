50b191d50077d5fb7bee81072e9d0ed714c4eab1
tikTokTik <mithoonk@flock.com>
2018-01-18 18:23:21 +0530

    special type of modifiers on events like enter keyup

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 new Vue({
   el:'#widget',
   data:{
-    name:'Mithoon Kumar',
-    website:"http://www.google.in",
-    tag:"<p>Hello</p>"
+
   },
   methods:{
-    greet:function(text){
-      console.log("consoling",this);// look at the object structure of this
-      return "Hey" + " " +text +" "+this._data.name + " "+ this.name ;
-    },
-    changeUrl:function(){
-      this.website = "https://atom.io/packages/terminal-plus";
-    },
-    click:function(){
-      alert("link clicked by user");
-    }
+     logName(){
+       alert("you entered the name");
+     }
   }
 });
