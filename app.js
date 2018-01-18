51118fb0d4297ae353dec6dcfd7ba3efba30f3db
tikTokTik <mithoonk@flock.com>
2018-01-18 20:18:34 +0530

    Revert "Difference between methods and computed see console logs"
    This reverts commit 58db01d493fd374550d6402ae8c204a93b9ab306.

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 new Vue({
   el:'#widget',
   data:{
-      a:0,
-      b:0
+      name:""
   },
   methods:{
-     methodA:function(){
-       console.log("executed methodA");
-       return this.a;
-     },
-     methodB:function(){
-       console.log("executed methodB");
-       return this.b;
+     logName(){
+       alert("you entered the name");
      }
-  },
-  computed:{
-    computedA:function(){
-      console.log("executed computedA");
-      return this.a;
-    },
-    computedB:function(){
-      console.log("executed computedB");
-      return this.b;
-    }
   }
 });
