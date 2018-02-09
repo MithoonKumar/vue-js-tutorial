4503ef74c4939a5dfc0daa80ce71f01df0989d79
tikTokTik <mithoonk@flock.com>
2018-02-09 12:48:24 +0530

    Add refs to any element

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-Vue.component('reusable', {
-  template:'<p>Hello People ! This is a reusable template</p>'
-});
-
-
 var first = new Vue({
   el:'#first-instance',
   data:{
     title: "first instance title unchanged"
-  }
-});
-
-var second = new Vue({
-  el: '#second-instance',
-  data: {
-    title: "second instance title unchanged"
   },
   methods: {
-    changeTitle: function(){
-      first.title = "title changed by the second instance";
+    readRefs(){
+      console.log(this.$refs.first.innerHTML);
     }
   }
 });
