f4392b876bc7c440fb1fab9b48889039f4b17f49
tikTokTik <mithoonk@flock.com>
2018-02-18 12:58:13 +0530

    Add slots

--- vue-cli-project/vue-cli-project/src/App.vue
+++ vue-cli-project/vue-cli-project/src/App.vue
@@ -1,30 +1,24 @@
 <template>
   <div>
-    <app-header v-bind:title="title"></app-header>
-    <app-content v-bind:arr="arr"></app-content>
-    <app-footer v-bind:title="title"></app-footer>
+    <new1>
+      <p slot="upper-slot">{{upperTitle}}</p>
+      <p slot="lower-slot">{{lowerTitle}}</p>
+    </new1>
   </div>
 </template>
 
 <script>
-import header from "./header1.vue";
-import content from "./body1.vue";
-import footer from "./footer1.vue"
+import new1 from "./new.vue"
 export default {
   components:{
-    'app-header': header,
-    'app-content': content,
-    'app-footer': footer
+    "new1": new1
   },
   name: 'app',
   data () {
     return {
-      title:'Title given from App.vue',
-      arr: ["tom", "den", "hary"]
+      upperTitle: "title in the upper slot",
+      lowerTitle: "title in the lower slot"
     }
-  },
-  created () {
-    console.log("created again");
   }
 }
 </script>
